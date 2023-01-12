2a021f8acf0d4dfea3f9b28e72a79a021b95cb86
robCrawford <robCrawford@users.noreply.github.com>
2023-01-12 07:07:16 +0000

    Demo

--- spelling/index.js
+++ spelling/index.js
@@ -14,25 +14,38 @@
 
   const helpHtml = `<p>Completing all ${config.fieldCount} words earns ${config.completedFieldsReward.toFixed(2)} points!</p><p>A word hint will be shown in the field if you need to click repeat ${config.hintCount} times.</p>`;
 
+  // Entries here will be the only words tested
+  let tempOverrideWords = [];
+
+  // Double click page title to enter overrides into localStorage
+  const overridesKey = `${config.stateName}-overrides`;
+  const storedOverrides = (localStorage.getItem(overridesKey) || '').replace(/,\s*/g, '|').split('|').filter(Boolean);
+  if (storedOverrides.length) {
+    tempOverrideWords = storedOverrides;
+  }
+
+  const additionalWords = ["guitar", "guide", "guidebook", "guardian", "guess", "guest", "guarantee"];
+
   const year2 = [
-    'about', 'above', 'after', 'again', 'although', 'always', 'another', 'ask', 'asked', 'baby', 'because', 'before', 'behind', 'between', 'both', 'call', 'called', 'children', 'climb', 'could', 'different', 'even', 'ever', 'every', 'everyone', 'everything', 'father', 'finally', 'friends', 'great', 'help', 'hide', 'house', 'know', 'large', 'last', 'little', 'looked', 'love', 'many', 'most', 'mother', 'Mr', 'Mrs', 'need', 'next', 'oh', 'once', 'only', 'other', 'our', 'over', 'people', 'please', 'really', 'school', 'should', 'small', 'suddenly', 'these', 'things', 'think', 'those', 'thought', 'thorough', 'time', 'together', 'under', 'until', 'very', 'where', 'which', 'work', 'would', 'year', 'young', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    "about", "above", "after", "again", "although", "always", "another", "ask", "asked", "baby", "because", "before", "behind", "between", "both", "call", "called", "children", "climb", "could", "different", "even", "ever", "every", "everyone", "everything", "father", "finally", "friends", "great", "help", "hide", "house", "know", "large", "last", "little", "looked", "love", "many", "most", "mother", "Mr", "Mrs", "need", "next", "oh", "once", "only", "other", "our", "over", "people", "please", "really", "school", "should", "small", "suddenly", "these", "things", "think", "those", "thought", "thorough", "time", "together", "under", "until", "very", "where", "which", "work", "would", "year", "young", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
   ];
 
   const year3 = [
-    'across', 'almost', 'along', 'animal', 'around', 'balloon', 'beautiful', 'began', 'being', 'below', 'better', 'birthday', 'break', 'brother', 'brought', 'change', 'child', 'Christmas', 'clothes', 'cold', 'coming', 'does', 'door', 'during', 'eyes', 'father', 'follow', 'found', 'garden', 'goes', 'gold', 'gone', 'grass', 'half', 'happy', 'head', 'high', 'hour', 'important', 'improve', 'inside', 'jumped', 'knew', 'lady', 'leave', 'light', 'might', 'mind', 'money', 'morning', 'move', 'much', 'near', 'never', 'number', 'opened', 'outside', 'own', 'paper', 'parents', 'place', 'pretty', 'prove', 'right', 'round', 'second', 'show', 'sister', 'something', 'sometimes', 'sound', 'started', 'still', 'stopped', 'such', 'sugar', 'sure', 'swimming', 'today', 'told', 'tries', 'turn', 'upon', 'used', 'walk', 'watch', 'water', 'while', 'white', 'whole', 'why', 'window', 'without', 'woke', 'woken', 'word', 'world', 'write', 'year', 'yellow'
+    "across", "almost", "along", "animal", "around", "balloon", "beautiful", "began", "being", "below", "better", "birthday", "break", "brother", "brought", "change", "child", "Christmas", "clothes", "cold", "coming", "does", "door", "during", "eyes", "father", "follow", "found", "garden", "goes", "gold", "gone", "grass", "half", "happy", "head", "high", "hour", "important", "improve", "inside", "jumped", "knew", "lady", "leave", "light", "might", "mind", "money", "morning", "move", "much", "near", "never", "number", "opened", "outside", "own", "paper", "parents", "place", "pretty", "prove", "right", "round", "second", "show", "sister", "something", "sometimes", "sound", "started", "still", "stopped", "such", "sugar", "sure", "swimming", "today", "told", "tries", "turn", "upon", "used", "walk", "watch", "water", "while", "white", "whole", "why", "window", "without", "woke", "woken", "word", "world", "write", "year", "yellow"
   ];
 
   const year4 = [
-    'accidentally', 'actually', 'occasionally', 'probably', 'knowledge', 'knowledgeable', 'words', 'mention', 'occasion', 'position', 'possession', 'question', 'possess', 'caught', 'naughty', 'eighth', 'reign', 'weight', 'height', 'therefore', 'famous', 'various', 'possible', 'enough', 'bicycle', 'business', 'disappear', 'disbelieve', 'rebuild', 'reposition', 'favourite', 'interest', 'library', 'ordinary', 'separate', 'address', 'appear', 'arrive', 'difficult', 'opposite', 'pressure', 'suppose', 'decide', 'describe', 'extreme', 'guide', 'surprise', 'earth', 'fruit', 'heart', 'history', 'increase', 'minute', 'natural', 'quarter', 'regular', 'material', 'experiment', 'length', 'center', 'century', 'certain', 'circle', 'exercise', 'experience', 'medicine', 'notice', 'recent', 'answer', 'breath', 'breathe', 'build', 'calendar', 'complete', 'consider', 'continue', 'early', 'group', 'guard', 'forwards', 'heard', 'imagine', 'island', 'learn', 'often', 'particular', 'peculiar', 'perhaps', 'popular', 'potatoes', 'promise', 'purpose', 'remember', 'centered', 'straight', 'strange', 'strength', 'woman', 'women'
+    "accidentally", "actually", "occasionally", "probably", "knowledge", "knowledgeable", "words", "mention", "occasion", "position", "possession", "question", "possess", "caught", "naughty", "eighth", "reign", "weight", "height", "therefore", "famous", "various", "possible", "enough", "bicycle", "business", "disappear", "disbelieve", "rebuild", "reposition", "favourite", "interest", "library", "ordinary", "separate", "address", "appear", "arrive", "difficult", "opposite", "pressure", "suppose", "decide", "describe", "extreme", "guide", "surprise", "earth", "fruit", "heart", "history", "increase", "minute", "natural", "quarter", "regular", "material", "experiment", "length", "center", "century", "certain", "circle", "exercise", "experience", "medicine", "notice", "recent", "answer", "breath", "breathe", "build", "calendar", "complete", "consider", "continue", "early", "group", "guard", "forwards", "heard", "imagine", "island", "learn", "often", "particular", "peculiar", "perhaps", "popular", "potatoes", "promise", "purpose", "remember", "centered", "straight", "strange", "strength", "woman", "women"
   ];
 
-  const allWords = [...year2, ...year3, ...year4];
+  // Words
+  const allWords = deduplicate(tempOverrideWords.length ? tempOverrideWords : [...year2, ...year3, ...year4, ...additionalWords]);
   const spellingState = JSON.parse(localStorage.getItem(config.stateName) || '{}');
   const incompleteWords = allWords.filter(word => !((spellingState[word] || 0) >= config.completedWordCount));
 
   // Rewards
   const completedFieldsCount = Object.values(spellingState).reduce((acc, count) => acc + count, 0) / config.fieldCount;
-  const rewardAmount = `🌟 ${(completedFieldsCount * config.completedFieldsReward).toFixed(2)}`;
+  const rewardAmount = `🌟 ${(round(completedFieldsCount * config.completedFieldsReward, config.completedFieldsReward)).toFixed(2)}`;
 
   let shuffledWords = incompleteWords
     .map(value => ({ value, sort: Math.random() }))
@@ -109,14 +122,19 @@
   }
 
   const fieldsHtml = words.map((word, i) =>
-    `<div class="field"><input id="${
-      wordToId(word)
-    }" type="text" autocorrect="off" autocapitalize="off" onfocus="speak('${
-      word
-    }')" onblur="updateResults()" /><span title="repeat" class="repeat" onclick="speak('${
-      word
-    }', true)">↻</span></div>`
+    `<div class="field"><input id="${wordToId(word)}" type="text" autocorrect="off" autocapitalize="off" /><span title="repeat" class="repeat">↻</span></div>`
   ).join('');
+
+  setTimeout(() => {
+    words.forEach((word) => {
+      const fieldEl = $(`#${wordToId(word)}`);
+      fieldEl.onfocus = () => speak(word);
+      fieldEl.onblur = () => updateResults();
+
+      const repeatEl = $(`#${wordToId(word)} + .repeat`);
+      repeatEl.onclick = () => speak(word, true);
+    });
+  });
 
   const resultsHtml = Object.entries(spellingState).map(([word, count]) => `<div class="results-word${count >= config.completedWordCount ? ' completed-word' : ''}"><h3>${word}</h3><span>${count}</span></div>`).join('');
 
@@ -130,6 +148,16 @@
   $('#results-title').onclick = () => {
     updateResultsUI(false);
   };
+  $('#title').ondblclick = () => {
+    const overrides = prompt('Words override list (comma separated, leave empty to clear!)');
+    if (overrides) {
+      localStorage.setItem(overridesKey, overrides);
+    }
+    else {
+      localStorage.removeItem(overridesKey);
+    }
+    location.reload();
+  }
 
   function updateResultsUI(showResults, hideTitle) {
     $('#results-title').style.display = showResults && !hideTitle ? 'block' : 'none';
@@ -137,6 +165,7 @@
     $('#results-link').style.display = showResults ? 'none' : 'block';
   }
 
+  // Listeners
   document.addEventListener("keyup", e => {
     if(e.key === 'Enter') {
       document.activeElement.blur();
@@ -146,5 +175,15 @@
   document.addEventListener('contextmenu', e => {
     e.preventDefault();
   });
+
+  // Utils
+  function deduplicate(array) {
+    return [...new Set(array)];
+  }
+
+  function round(value, step = 1) {
+    var inv = 1.0 / step;
+    return Math.round(value * inv) / inv;
+  }
 
 }());
