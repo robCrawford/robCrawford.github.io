3f7f05a39d7a4d0349b98eee92a92c663c20b028
robCrawford <robCrawford@users.noreply.github.com>
2023-03-05 13:02:14 +0000

    404 page

--- spelling/multiplication/data.js
+++ spelling/multiplication/data.js
@@ -5,10 +5,12 @@ export const numberWords = [
   '', '', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'
 ];
 
+const testedTables = [3, 4, 5, 6, 7, 8, 9, 11, 12];
+
 export default {
   tables: numberWords.flatMap((word, int) => {
     const table = [];
-    if (word) {
+    if (word && testedTables.includes(int)) {
       for (let i = firstMultiplier; i < lastMultiplier + 1; i++) {
         table.push(`${i} x ${int} = ${i * int}`);
       };
