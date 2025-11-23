f39071c020b5150f0a8518ea4b6c7a19e4aa64e0
Robert-Crawford-xa9_nbcuni <robert.crawford2@sky.uk>
2025-11-23 11:18:49 +0000

    Words

--- spelling/spelling/index.js
+++ spelling/spelling/index.js
@@ -17,7 +17,7 @@ let name = '';
 
 // Entries here will be the only words tested
 // NOTE: update fieldCount for rewards calculation!
-let tempOverrideWords = ['was', 'they', 'on', 'you', 'she'];
+let tempOverrideWords = ['at', 'but', 'is', 'his', 'for'];
 
 // Clear local storage prior to latest key
 // NOTE: changing this removes all previous data from the browser!
