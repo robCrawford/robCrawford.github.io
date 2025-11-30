84d1ceaf6b008096db14bbbcb5486ab0e3881f2f
Robert-Crawford-xa9_nbcuni <robert.crawford2@sky.uk>
2025-11-30 16:20:47 +0000

    Words

--- spelling/index.js
+++
@@ -1,7 +0,0 @@
-import { initSpelling } from './spelling/index.js';
-
-window.scrollTo(0, 0);
-
-window.$ = selector => document.querySelector(selector);
-
-initSpelling();
--- spelling/spelling/index.js
+++ spelling/spelling/index.js
@@ -17,7 +17,7 @@ let name = '';
 
 // Entries here will be the only words tested
 // NOTE: update fieldCount for rewards calculation!
-let tempOverrideWords = ['at', 'but', 'is', 'his', 'for'];
+let tempOverrideWords = ['we', 'all', 'with', 'that', 'can'];
 
 // Clear local storage prior to latest key
 // NOTE: changing this removes all previous data from the browser!
