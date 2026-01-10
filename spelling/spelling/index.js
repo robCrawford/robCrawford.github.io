52c7d8678bf7561b4148dee657495325da7272ef
Robert-Crawford-xa9_nbcuni <robert.crawford2@sky.uk>
2026-01-10 20:16:32 +0000

    Spelling

--- spelling/spelling/index.js
+++ spelling/spelling/index.js
@@ -19,7 +19,7 @@ let name = '';
 
 // Entries here will be the only words tested
 // NOTE: update fieldCount for rewards calculation!
-let tempOverrideWords = [];
+let tempOverrideWords = ['said', 'you', 'they', 'we', 'all', 'with'];
 
 // Clear local storage prior to latest key
 // NOTE: changing this removes all previous data from the browser!
